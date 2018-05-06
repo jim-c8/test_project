8ef051e99ded3c156c05eb2ac9b3807692729f06
jim-c8 <jimchiappe@yahoo.com>
2018-05-06 00:13:45 -0700

    original file

--- test_project.py
+++ test_project.py
@@ -1,13 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print('test project line 1')
 print('test project line 2')
 print('test project - end')
 
 print('this is the new line added to the branch')
 print('adding a second line')
-
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1, 500)
 y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
