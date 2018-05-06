bdf4699667165fdecbb7e12e94f72c6139d0cba1
jim-c8 <jimchiappe@yahoo.com>
2018-05-05 23:55:07 -0700

    original file

--- test_project.py
+++ test_project.py
@@ -1,8 +1,19 @@
-import matplotlib
-
 print('test project line 1')
 print('test project line 2')
 print('test project - end')
 
 print('this is the new line added to the branch')
 print('adding a second line')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 1, 500)
+y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
+
+fig, ax = plt.subplots()
+
+ax.fill(x, y, zorder=10)
+ax.grid(True, zorder=5)
+plt.show()
